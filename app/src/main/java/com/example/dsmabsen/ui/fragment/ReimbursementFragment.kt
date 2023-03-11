5712692fdbaf1a4d513baf813f21f58a1ff09b5d
damha <damhadamha47@gmail.com>
2023-03-11 23:56:49 +0700

    gas

--- app/src/main/java/com/example/dsmabsen/ui/fragment/ReimbursementFragment.kt
+++ app/src/main/java/com/example/dsmabsen/ui/fragment/ReimbursementFragment.kt
@@ -1,10 +1,11 @@
 package com.example.dsmabsen.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.example.dsmabsen.R
 import com.example.dsmabsen.databinding.FragmentReimbursementBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,8 +16,56 @@ class ReimbursementFragment : BaseFragment<FragmentReimbursementBinding>(Fragmen
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        setupToolbar("Reimbursement")
+        view.findViewById<Toolbar>(R.id.toolbar)?.let { toolbar ->
+            toolbar.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.add -> {
+                        // Handle add menu item click
+                        Toast.makeText(context, "Add di Click", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        val menuSave = menu.findItem(R.id.save)
+        val menuPlus = menu.findItem(R.id.add)
+
+//        menuSave?.isVisible = false // menyembunyikan menu tertentu
+        menuPlus?.isVisible = false // menyembunyikan menu tertentu
+
+        val item = menu.findItem(R.id.save)
+        item.setActionView(R.layout.item_menu_toolbar)
+
+        val actionView = item.actionView
+        val btnSimpan = actionView?.findViewById<TextView>(R.id.textSimpan)
+        btnSimpan?.setOnClickListener {
+            // your code here
+            Toast.makeText(context, "Save di Click", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add -> {
+                // Handle add menu item click
+                Toast.makeText(context, "Add di Click", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.save -> {
+                // Handle navigation button click
+                Toast.makeText(context, "Save di Click", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
