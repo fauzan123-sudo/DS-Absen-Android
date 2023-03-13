ebfc4330f3049ff4e1556c6823d7387c8a85006c
damha <damhadamha47@gmail.com>
2023-03-13 07:07:32 +0700

    a

--- app/src/main/java/com/example/dsmabsen/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/dsmabsen/ui/fragment/ProfileFragment.kt
@@ -3,7 +3,10 @@ package com.example.dsmabsen.ui.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
@@ -34,69 +37,21 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
     @Inject
     lateinit var tokenManager: TokenManager
+    val savedUser = Paper.book().read<DataX>("user")
+    val nipUser = savedUser!!.nip
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val savedUser = Paper.book().read<DataX>("user")
-        val nipUser = savedUser!!.nip
+
+        setHasOptionsMenu(true)
+        setupToolbar("Profile")
 
         with(binding) {
             rvProfileLengkap.setOnClickListener {
                 findNavController().navigate(R.id.action_profileFragment_to_allProfileFragment)
             }
 
-
-
-            llLogout.setOnClickListener {
-                AlertDialog.Builder(requireContext())
-                    .setTitle("LogOut")
-                    .setMessage("Anda Yakin ingin logout")
-                //    .setIcon(R.drawable.ic_warning)
-                    .setPositiveButton("Ya"){ _, _ ->
-                        val nips = RequestNip(
-                            nipUser
-                        )
-
-                        Toast.makeText(requireContext(), "$nips", Toast.LENGTH_SHORT).show()
-                        viewModels.requestLogout(nipUser)
-                        viewModels.logOutLiveData.observe(viewLifecycleOwner) {
-                            binding.loadingInclude.loading.isVisible = false
-                            when (it) {
-                                is NetworkResult.Success -> {
-                                    val data = it.data!!
-                                    binding.loadingInclude.loading.isVisible= false
-                                    binding.constraintLayout3.isVisible = true
-                                    requireActivity().startActivity(
-                                        Intent(
-                                            requireActivity(),
-                                            LoginActivity::class.java
-                                        )
-                                    )
-                                    Paper.book().delete("user")
-                                    tokenManager.deleteToken()
-                                }
-
-                                is NetworkResult.Loading -> {
-                                    binding.constraintLayout3.isVisible = false
-                                    binding.loadingInclude.loading.isVisible = true
-                                }
-
-                                is NetworkResult.Error -> {
-                                    handleApiError(it.message)
-                                    Log.d("TAG", it.message.toString())
-                                    binding.loadingInclude.loading.isVisible = false
-//                            Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_LONG).show()
-                                }
-                            }
-                        }
-                    }
-                    .setNegativeButton("Kembali"){_,_ ->
-//                        Toast.makeText(requireContext(), "Kembali", Toast.LENGTH_SHORT).show()
-                    }
-                    .create().show()
-
-            }
         }
 
         viewModel.profileUserRequest(savedUser!!.nip)
@@ -130,5 +85,75 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
                 }
             }
         }
+    }
+
+    private fun logout(nipUser: String) {
+        AlertDialog.Builder(requireContext())
+            .setTitle("LogOut")
+            .setMessage("Anda Yakin ingin logout")
+            //    .setIcon(R.drawable.ic_warning)
+            .setPositiveButton("Ya") { _, _ ->
+                val nips = RequestNip(
+                    nipUser
+                )
+
+                Toast.makeText(requireContext(), "$nips", Toast.LENGTH_SHORT).show()
+                viewModels.requestLogout(nipUser)
+                viewModels.logOutLiveData.observe(viewLifecycleOwner) {
+                    binding.loadingInclude.loading.isVisible = false
+                    when (it) {
+                        is NetworkResult.Success -> {
+                            val data = it.data!!
+                            binding.loadingInclude.loading.isVisible = false
+                            binding.constraintLayout3.isVisible = true
+                            requireActivity().startActivity(
+                                Intent(
+                                    requireActivity(),
+                                    LoginActivity::class.java
+                                )
+                            )
+                            Paper.book().delete("user")
+                            tokenManager.deleteToken()
+                        }
+
+                        is NetworkResult.Loading -> {
+                            binding.constraintLayout3.isVisible = false
+                            binding.loadingInclude.loading.isVisible = true
+                        }
+
+                        is NetworkResult.Error -> {
+                            handleApiError(it.message)
+                            Log.d("TAG", it.message.toString())
+                            binding.loadingInclude.loading.isVisible = false
+    //                            Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+            .setNegativeButton("Kembali") { _, _ ->
+    //                        Toast.makeText(requireContext(), "Kembali", Toast.LENGTH_SHORT).show()
+            }
+            .create().show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        val menuSave = menu.findItem(R.id.save)
+        val menuPlus = menu.findItem(R.id.add)
+
+        menuSave?.isVisible = false // menyembunyikan menu tertentu
+        menuPlus?.isVisible = false // menyembunyikan menu tertentu
+
+        val item = menu.findItem(R.id.save)
+        item.setActionView(R.layout.item_menu_toolbar)
+
+        val actionView = item.actionView
+        val btnSimpan = actionView?.findViewById<TextView>(R.id.textSimpan)
+        val btnLogout = actionView?.findViewById<TextView>(R.id.textLogout)
+        btnLogout!!.isVisible = true
+        btnLogout?.setOnClickListener {
+            logout(nipUser)
+        }
+
     }
 }
