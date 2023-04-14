41f991a4578b9e626624c4fc6fd821f05374d824
damha <damhadamha47@gmail.com>
2023-04-15 02:34:45 +0700

    memperbaiki perizinan dan handle jika perizinan ditolak secara permanen

--- app/src/main/java/com/infinity/dsmabsen/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/infinity/dsmabsen/ui/fragment/HomeFragment.kt
@@ -1,14 +1,12 @@
 package com.infinity.dsmabsen.ui.fragment
 
 import android.Manifest
-import android.app.Activity
-import android.content.Context
-import android.content.Context.TELEPHONY_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.telephony.TelephonyManager
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -18,71 +16,54 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import com.infinity.dsmabsen.R
 import com.infinity.dsmabsen.databinding.FragmentHomeBinding
 import com.infinity.dsmabsen.ui.activity.LoginActivity
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding: FragmentHomeBinding
-
-
-    companion object {
-        const val ALL_PERMISSIONS_CODE = 1
-    }
-
-    val permissions = arrayOf(
+    private lateinit var binding: FragmentHomeBinding
+    private val permissions = arrayOf(
         Manifest.permission.CAMERA,
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.READ_PHONE_STATE,
         Manifest.permission.READ_EXTERNAL_STORAGE,
     )
-
+    private val ALL_PERMISSIONS_CODE = 1
+    private val PERMISSION_DENIED_PERMANENTLY_CODE = 2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        if (!checkAllPermissionsGranted(permissions)) {
-            binding.requestPermissionsButton.visibility = View.VISIBLE
-
-            binding.requestPermissionsButton.setOnClickListener {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    permissions,
-                    ALL_PERMISSIONS_CODE
-                )
-            }
-        } else {
-            binding.requestPermissionsButton.visibility = View.GONE
-
-            startLoginActivity()
-        }
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            if (checkAllPermissionsGranted(permissions)) {
-                requestPermissionsButton.visibility = View.GONE
+
+        binding.requestPermissionsButton.setOnClickListener {
+            if (!checkAllPermissionsGranted(permissions)) {
+                requestPermissions(permissions, ALL_PERMISSIONS_CODE)
             } else {
-                requestPermissionsButton.visibility = View.VISIBLE
-                requestPermissionsButton.setOnClickListener {
-                    requestPermissions(permissions, ALL_PERMISSIONS_CODE)
-                }
+                startLoginActivity()
             }
+        }
+        if (!checkAllPermissionsGranted(permissions)) {
+            requestPermissions(permissions, ALL_PERMISSIONS_CODE)
+        } else {
+            startLoginActivity()
         }
     }
 
-    private fun checkAllPermissionsGranted(permissions: Array<String>): Boolean {
+    private fun checkAllPermissionsGranted(permissions: Array<out String>): Boolean {
         for (permission in permissions) {
-            if (ContextCompat.checkSelfPermission(requireContext(), permission)
-                != PackageManager.PERMISSION_GRANTED
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
             ) {
                 return false
             }
@@ -97,31 +78,67 @@ class HomeFragment : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == ALL_PERMISSIONS_CODE) {
-            var allGranted = true
-            for (grantResult in grantResults) {
-                if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                    allGranted = false
-                    break
-                }
-            }
-            for (i in permissions.indices) {
-                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                    allGranted = false
-                    Log.d("Perizinan", "Permission denied: ${permissions[i]}")
-                    break
+        when (requestCode) {
+            ALL_PERMISSIONS_CODE -> {
+                if (checkAllPermissionsGranted(permissions)) {
+                    startLoginActivity()
                 } else {
-                    Log.d("Perizinan", "Permission granted: ${permissions[i]}")
+                    // check if any permission was denied permanently
+                    var isAnyPermissionDeniedPermanently = false
+                    for (i in permissions.indices) {
+                        if (!shouldShowRequestPermissionRationale(permissions[i]) &&
+                            grantResults[i] != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            isAnyPermissionDeniedPermanently = true
+                            break
+                        }
+                    }
+                    if (isAnyPermissionDeniedPermanently) {
+                        // show an explanation dialog to the user
+                        showExplanationDialog(PERMISSION_DENIED_PERMANENTLY_CODE)
+                    } else {
+                        // show rationale and request permissions again
+                        showExplanationDialog(ALL_PERMISSIONS_CODE)
+                    }
                 }
             }
-            if (allGranted) {
-                binding.requestPermissionsButton.visibility = View.GONE
-
-                startLoginActivity()
-            } else {
-                Toast.makeText(requireContext(), "Permissions denied", Toast.LENGTH_SHORT).show()
+            PERMISSION_DENIED_PERMANENTLY_CODE -> {
+                // open device settings so that user can enable the permissions
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", requireContext().packageName, null)
+                intent.data = uri
+                startActivity(intent)
             }
         }
+    }
+
+    private fun showExplanationDialog(requestCode: Int) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("aplikasi membutuhkan perizinan")
+        builder.setMessage("aplikasi membutuhakn beberapa perizinn agar dapat digunakan")
+
+        builder.setPositiveButton("buka pengaturan") { _, _ ->
+            if (requestCode == PERMISSION_DENIED_PERMANENTLY_CODE) {
+                // open device settings so that user can enable the permissions
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", requireContext().packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } else {
+                // show rationale and request permissions again
+                requestPermissions(permissions, ALL_PERMISSIONS_CODE)
+            }
+        }
+
+        builder.setNegativeButton("batal") { _, _ ->
+            Toast.makeText(
+                requireContext(),
+                "perizinan dibatalkan",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.show()
     }
 
     private fun startLoginActivity() {
@@ -129,3 +146,4 @@ class HomeFragment : Fragment() {
         requireActivity().finish()
     }
 }
+
